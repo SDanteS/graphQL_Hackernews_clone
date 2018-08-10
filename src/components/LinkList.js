775ba8f34d0a94c61beba138f52b348af8db6136
Sofiane <stamboul.sofiane@gmail.com>
2018-08-10 17:01:21 +0200

    harckernews clone with apollo client

---
+++ src/components/LinkList.js
@@ -0,0 +1,132 @@
+import React from "react";
+import Link from "./Link";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+export const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      node {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      node {
+        id
+        link {
+          id
+          url
+          description
+          createdAt
+          postedBy {
+            id
+            name
+          }
+          votes {
+            id
+            user {
+              id
+            }
+          }
+        }
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+const LinkList = () => {
+  const _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+  const _subscribeToNewLinks = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink.node;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        });
+      }
+    });
+  };
+  const _subscribeToNewVotes = async subscribeToMore => {
+    subscribeToMore({ document: NEW_VOTES_SUBSCRIPTION });
+  };
+
+  return (
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data, subscribeToMore }) => {
+        if (loading) return <div>Fetching</div>;
+        if (error) return <div>Error</div>;
+
+        _subscribeToNewLinks(subscribeToMore);
+        _subscribeToNewVotes(subscribeToMore);
+
+        const linksToRender = data.feed.links;
+        return linksToRender.map((link, index) => (
+          <Link
+            key={link.id}
+            link={link}
+            index={index}
+            updateStoreAfterVote={_updateCacheAfterVote}
+          />
+        ));
+      }}
+    </Query>
+  );
+};
+
+export default LinkList;
